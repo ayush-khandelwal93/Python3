f9d2481d00fbd1e0f336d86522940f13ef22e4c5
Ayush Khandelwal <ayushkhandelwal93@gmail.com>
2018-12-29 17:30:17 +0530

    default values passed to the class, user has an option to give its value or use the default one

--- OOP/blob_class.py
+++ OOP/blob_class.py
@@ -1,17 +1,18 @@
 import random
 class Blob:
     
-    def __init__(self,color,x_boundary,y_boundary,size_range=(4,8)):
+    def __init__(self,color,x_boundary,y_boundary,size_range=(4,8), movement_range=(-1,2)):
         self.x_boundary = x_boundary
         self.y_boundary = y_boundary
         self.x=random.randrange(0,self.x_boundary)
         self.y=random.randrange(0,self.y_boundary)
         self.size=random.randrange(size_range[0],size_range[1])
         self.color=color
+        self.movement_range=movement_range
 
     def move(self):
-        self.move_x = random.randrange(-1,2)
-        self.move_y = random.randrange(-1,2)
+        self.move_x = random.randrange(self.movement_range[0],self.movement_range[1])
+        self.move_y = random.randrange(self.movement_range[0],self.movement_range[1])
         self.x += self.move_x
         self.y += self.move_y
 
