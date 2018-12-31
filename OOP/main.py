07225a008427cd9d71a0232ee17c3b26ac53abba
Ayush Khandelwal <ayushkhandelwal93@gmail.com>
2018-12-31 18:02:17 +0530

    overloading completed and graphical verison exhibited

--- OOP/main.py
+++ OOP/main.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 from blob_class import Blob
+import numpy as np
 
 WIDTH = 800
 HEIGHT = 600
@@ -8,9 +9,9 @@ WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 
-STARTING_BLUE_BLOBS = 10
-STARTING_RED_BLOBS = 3
-STARTING_GREEN_BLOBS = 5
+STARTING_BLUE_BLOBS = 30
+STARTING_RED_BLOBS = 30
+STARTING_GREEN_BLOBS = 25
 
 game_display = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption('Blob World')
@@ -42,8 +43,30 @@ class GreenBlob(Blob):
     def __init__(self, x_boundary,y_boundary):
         super().__init__((0,255,0),x_boundary,y_boundary)
 
+def is_touching(b1,b2):
+    return np.linalg.norm(np.array([b1.x,b1.y])-np.array([b2.x,b2.y])) <(b1.size+b2.size)
+
+def handle_collisions(blob_list):
+    blues, reds, greens=blob_list
+    for blue_id, blue_blob in blues.copy().items():
+        for other_blobs in blues, reds, greens:
+            for other_blob_id, other_blob in other_blobs.copy().items():
+                if blue_blob==other_blob:
+                    pass
+                else:
+                    if is_touching(blue_blob,other_blob):
+                        blue_blob+other_blob
+                        if other_blob.size<=0:
+                            del other_blobs[other_blob_id]
+                        if blue_blob.size<=0:
+                            del blues[blue_id]
+
+    return [blues, reds, greens]
+
+
 def draw_environment(blob_list):
     game_display.fill(WHITE)
+    blob_list=handle_collisions(blob_list)
     for blob_dict in blob_list:
         for blob_id in blob_dict:
             blob=blob_dict[blob_id]
@@ -51,26 +74,22 @@ def draw_environment(blob_list):
             blob.move()
             blob.check_bounds()
     pygame.display.update()
+    return blob_list
 
 def main():
     blue_blobs=dict(enumerate([BlueBlob(WIDTH,HEIGHT) for i in range(STARTING_BLUE_BLOBS)]))
     red_blobs=dict(enumerate([RedBlob(WIDTH,HEIGHT) for i in range(STARTING_RED_BLOBS)]))
     green_blobs=dict(enumerate([GreenBlob(WIDTH,HEIGHT) for i in range(STARTING_GREEN_BLOBS)]))
 
-    print('Current blue size: {}. Current red size: {}'.format(str(blue_blobs[0].size),
-                                                               str(red_blobs[0].size)))
 
-    blue_blobs[0] + red_blobs[0]
-    print('Current blue size: {}. Current red size: {}'.format(str(blue_blobs[0].size),str(red_blobs[0].size)))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
 
-    #while True:
-    #    for event in pygame.event.get():
-    #        if event.type == pygame.QUIT:
-    #            pygame.quit()
-    #            quit()
-
-    #    draw_environment([blue_blobs,red_blobs,green_blobs])
-    #    clock.tick(60)
+        [blue_blobs,red_blobs,green_blobs]=draw_environment([blue_blobs,red_blobs,green_blobs])
+        clock.tick(60)
 
 if __name__=='__main__':
     main()
