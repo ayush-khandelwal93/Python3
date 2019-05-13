0898efe725fdea1d2c2a530aefe5fedddb03097f
Ayush Khandelwal <ayushkhandelwal93@gmail.com>
2019-05-14 00:56:39 +0530

    spyder crawler made

---
+++ PageRank/spider.py
@@ -0,0 +1,144 @@
+import sqlite3
+import ssl
+from urllib.parse import urljoin
+from urllib.request import urlopen
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+conn = sqlite3.connect('spyder.sqlite')
+cur = conn.cursor()
+cur.execute('DROP TABLE IF EXISTS Pages')
+cur.execute('CREATE TABLE IF NOT EXISTS Pages\
+    (id INTEGER PRIMARY KEY, url TEXT UNIQUE, html TEXT,\
+     error INTEGER, old_rank REAL, new_rank REAL)')
+
+cur.execute(''' CREATE TABLE IF NOT EXISTS Links 
+	(from_id INTEGER, to_id INTEGER)''')
+
+cur.execute('''CREATE TABLE IF NOT EXISTS webs 
+	(url TEXT UNIQUE)''')
+
+cur.execute('SELECT id, url FROM Pages WHERE html is NULL and \
+	error is NULL ORDER BY RANDOM() LIMIT 1')
+
+row = cur.fetchone()
+if row is not None:
+	print('Restarting Old crawl, pages are there in db yet to be cra\
+wled')
+else:
+	starturl = input('Enter web url')
+	if len(starturl) < 1:
+		starturl = 'http://www.dr-chuck.com/'
+	if starturl.endswith('/'):
+		starturl = starturl[:-1]
+	web = starturl
+	if starturl.endswith('.htm') or starturl.endswith('.html'):
+		pos = starturl.rfind('/')
+		web = starturl[:pos]
+	if len(web) > 1:
+		cur.execute('DELETE FROM Pages WHERE url = ?',(starturl,))
+		conn.commit()
+		cur.execute('INSERT OR IGNORE INTO webs (url) VALUES \
+		(?)', (web,))
+		cur.execute('INSERT OR IGNORE INTO Pages (url, html,new_rank\
+		) VALUES (?, NULL, 1.0)', (starturl,))
+		conn.commit()
+
+cur.execute('SELECT url FROM webs')
+webs = list()
+for row in cur:
+	webs.append(str(row[0]))
+print(webs)
+
+many = 0
+while True:
+	if many < 1:
+		sval = input('How many pages:')
+		if len(sval) < 1: break
+		many = int(sval)
+	many = many -1
+	cur.execute('SELECT id,url FROM Pages WHERE html is NULL and \
+	error is NULL ORDER BY RANDOM() LIMIT 1')
+	try:
+		row = cur.fetchone()
+		fromid = row[0]
+		url = row[1]
+	except:
+		print('No HTML pages found')
+	
+	try:
+		document = urlopen(url, context = ctx)
+		html = document.read()
+		if document.getcode() != 200:
+			print('Error on page: ', document.getcode())
+			cur.execute('UPDATE Pages SET error = ? WHERE url = ?'\
+			,(document.getcode(),url))
+
+		if 'text/html' != document.info().get_content_type():
+			print('Ignore non text/html page')
+			cur.execute('DELETE FROM Pages WHERE url = ?' , (url,))
+			conn.commit()
+			continue
+		print('('+str(len(html))+')', end=' ')
+		soup = BeautifulSoup(html , 'html.parser')
+	except KeyboardInterrupt:
+		print('Program interrupted by user')
+	except:
+		print('Unable to retrieve or parse pages')
+		cur.execute('UPDATE Pages SET error=-1 WHERE url=?',(url,))
+		conn.commit()
+		continue
+	cur.execute('UPDATE Pages SET html = ? WHERE url= ?',(memoryview(html),\
+	url))
+	conn.commit()
+
+	tags = soup('a')
+	count = 0
+	for tag in tags:
+		href = tag.get('href',None)
+		if href is None: continue
+		#print(href)
+		up = urlparse(href)
+		#print(len(up.scheme))
+		if len(up.scheme) <1:
+			href = urljoin(url,href)
+		ipos = href.find('#')
+		if ipos > 1:
+			href = href[:ipos]
+		if href.endswith('.png') or href.endswith('.jpg') or href.endswith\
+		('.gif') : continue
+		if href.endswith('/') : href = href[:-1]
+		if len(href) < 1 : continue
+
+		found = False
+
+		for web in webs:
+			if href.startswith(web):
+				found = True
+				break
+		if not found : continue
+
+		cur.execute('INSERT OR IGNORE INTO Pages (url, html, new_rank) \
+		VALUES (?, NULL, 1.0)', (href,))
+		count = count+1
+		conn.commit()
+
+		cur.execute('SELECT id FROM Pages WHERE url = ? LIMIT 1',\
+		(href,))
+		try:
+			row = cur.fetchone()
+			toid = row[0]
+		except:
+			print('Could not,retrieve id')
+			continue
+		
+		cur.execute('INSERT OR IGNORE INTO Links (from_id,to_id) VALUES \
+		(?,?)', (fromid,toid))
+
+	print(count)
+
+cur.close()
