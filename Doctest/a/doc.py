7cac4338488a37f9bf186b29fcb665b6b3c2d24e
Ayush Khandelwal <ayushkhandelwal93@gmail.com>
2019-06-01 20:22:39 +0530

    doctest

---
+++ Doctest/a/doc.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.append('/home/ayush/Downloads/Python3/Doctest')
+
+def my_function(a, z):
+    """
+	>>> from k import b
+	>>> b.c
+	2
+    >>> my_function(2, 3)
+    6
+    """
+    return a * z 
